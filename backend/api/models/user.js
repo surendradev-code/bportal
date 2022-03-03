f97874f08dee26a453b31113365071df9c78452e
Surendradev-code <mk5414894@gmail.com>
2022-03-03 13:02:08 +0530

    update user

--- backend/api/models/user.js
+++ backend/api/models/user.js
@@ -1,6 +1,6 @@
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://meanproject:<mean123>@clustermean.jgqex.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {useNewUrlParser: true});
+mongoose.connect('mongodb+srv://meanproject:<password>@clustermean.jgqex.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {useNewUrlParser: true});
 var conn =mongoose.Collection;
 
 var userSchema =new mongoose.Schema({
